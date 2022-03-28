1e2cb2761fdd23b9089c48eb50238718b67bb98c
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-03-28 01:25:33 -0400

    more input validations in place

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const inquirer = require('inquirer');
 const connection = require('./config/connection')
 const DatabasePromptAPI = require('./lib/DatabasePromptAPI');
-const cTable = require('console.table');
 
 const dbAPI = new DatabasePromptAPI(connection);
 
-dbAPI.promiseColInfo('employee', false)
-.then((result)=>console.log(result))
 
-// dbAPI.startPrompts();
+
+
+// ------------------------------------//
+// dbAPI.promiseColInfo('employee', false)
+// .then((result)=>console.log(result.map(res => res.Field)))
+
+
 // dbAPI.addDepartment('test department1213')
 // .then(()=> dbAPI.printAllDepartments())
 
 
 
 
+dbAPI.startPrompts();
--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,19 @@
 module.exports = {
     getValues(object) {
         return Object.values(object).flat();
+    },
+
+    validateType(attempt, desiredType){
+        switch (desiredType){
+            case 'int':
+            case 'decimal(10,0)':
+                return !isNaN(attempt) && !attempt.includes(',');
+            case 'text':
+            case 'varchar(30)':
+                return isNaN(attempt) && typeof attempt == 'string';
+            default:
+                return attempt? true: false;
+        }
     }
 }
 
