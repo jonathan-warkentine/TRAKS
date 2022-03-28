3bf616e3cdc2811f25f9e7e8d952f4df2ca2fbea
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-03-28 02:35:41 -0400

    more tidying

--- index.js
+++ index.js
@@ -3,19 +3,4 @@ const DatabasePromptAPI = require('./lib/DatabasePromptAPI');
 
 const dbAPI = new DatabasePromptAPI(connection);
 
-
-
-
-// ------------------------------------//
-// dbAPI.promiseColInfo('employee', false)
-// .then((result)=>console.log(result.map(res => res.Field)))
-
-
-// dbAPI.addDepartment('test department1213')
-// .then(()=> dbAPI.printAllDepartments())
-
-
-// dbAPI.checkForeignConstraints('department', 10)
-// .then((result) => console.log(result))
-
 dbAPI.startPrompts();
